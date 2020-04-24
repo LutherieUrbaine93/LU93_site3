7e289b7cc3d0c5655746e3851ca8f5631296a4a5
Adrien <tasticad@gmail.com>
2020-04-24 16:26:00 +0200

    Update intro sound effects
    - change script to non-randomize

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
 jQuery(function ($) {
-  "use strict";
+	"use strict";
 
-  /* ========================================================================= */
-  /*  Page Preloader
-  /* ========================================================================= */
+	/* ========================================================================= */
+	/*  Page Preloader
+	/* ========================================================================= */
 
-  $(window).on('load', function () {
-    $('.preloader').fadeOut(500);
-  });
-
-
-  // lazy load initialize
-  const observer = lozad(); // lazy loads elements with default selector as ".lozad"
-  observer.observe();
+	$(window).on('load', function () {
+		$('.preloader').fadeOut(500);
+	});
 
 
+	// lazy load initialize
+	const observer = lozad(); // lazy loads elements with default selector as ".lozad"
+	observer.observe();
 
-  /* ========================================================================= */
-  /*  Sound effects
-  /* =========================================================================  */
-
-  $(".box").mouseenter(function() {
-      $("#sound-" + Math.ceil(Math.random() * 7))[0].play();
-  });
-
-
+	/* ========================================================================= */
+	/*  Sound effects
+	/* =========================================================================  */
+	/* === for random played sound-:
+	$(".box").mouseenter(function() {
+			$("#sound-" + Math.ceil(Math.random() * 7))[0].play();
+	}); ===  */
+	
+	var sound = $("#sound-")[0];
+	$(".box").mouseenter(function() {
+		sound.pause();
+		sound.play();
+	});
 
 
 /* ============================== end script ================================= */
